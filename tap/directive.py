ef69b6ab5063750177d10810baf196005d3128fe
Matt Layman <matthewlayman@gmail.com>
2014-12-23 00:02:39 -0500

    Add Directive to model directive comments.

---
+++ tap/directive.py
@@ -0,0 +1,45 @@
+# Copyright (c) 2014, Matt Layman
+
+import re
+
+
+class Directive(object):
+    """A representation of a result line directive."""
+
+    skip_pattern = re.compile(r'^SKIP\S*', re.IGNORECASE)
+    todo_pattern = re.compile(
+        r"""^TODO\b             # The directive name
+            (?P<whitespace>\s*) # Immediately following must be whitespace.
+            (?P<remainder>.*)   # Slurp up the rest.""",
+        re.IGNORECASE | re.VERBOSE)
+
+    def __init__(self, text):
+        """Initialize the directive by parsing the text.
+
+        The text is assumed to be everything after a '#\s*' on a result line.
+        """
+        self._skip = False
+        self._todo = False
+
+        match = self.skip_pattern.match(text)
+        if match:
+            self._skip = True
+
+        match = self.todo_pattern.match(text)
+        if match:
+            if match.group('whitespace'):
+                self._todo = True
+            else:
+                # Catch the case where the directive has no descriptive text.
+                if match.group('remainder') == '':
+                    self._todo = True
+
+    @property
+    def skip(self):
+        """Check if the directive is a SKIP type."""
+        return self._skip
+
+    @property
+    def todo(self):
+        """Check if the directive is a TODO type."""
+        return self._todo
---
+++ tap/tests/test_directive.py
@@ -0,0 +1,47 @@
+# Copyright (c) 2014, Matt Layman
+
+import unittest
+
+from tap.directive import Directive
+
+
+class TestDirective(unittest.TestCase):
+    """Tests for tap.directive.Directive"""
+
+    def test_finds_todo(self):
+        text = 'ToDo This is something to do.'
+        directive = Directive(text)
+
+        self.assertTrue(directive.todo)
+
+    def test_finds_simplest_todo(self):
+        text = 'TODO'
+        directive = Directive(text)
+
+        self.assertTrue(directive.todo)
+
+    def test_todo_has_boundary(self):
+        """TAP spec indicates TODO directives must be on a boundary."""
+        text = 'TODO: Not a TODO directive because of an immediate colon.'
+        directive = Directive(text)
+
+        self.assertFalse(directive.todo)
+
+    def test_finds_skip(self):
+        text = 'Skipping This is something to skip.'
+        directive = Directive(text)
+
+        self.assertTrue(directive.skip)
+
+    def test_finds_simplest_skip(self):
+        text = 'SKIP'
+        directive = Directive(text)
+
+        self.assertTrue(directive.skip)
+
+    def test_skip_at_beginning(self):
+        """Only match SKIP directives at the beginning."""
+        text = 'This is not something to skip.'
+        directive = Directive(text)
+
+        self.assertFalse(directive.skip)
