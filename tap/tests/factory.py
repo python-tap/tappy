f38f8765ba2b25437dddb5af13f68be3a6fdcffa
Matt Layman <matthewlayman@gmail.com>
2014-12-27 21:24:58 -0500

    Fix Python 2.6 and Python 3 tests.

--- tap/tests/factory.py
+++ tap/tests/factory.py
@@ -1,7 +1,11 @@
 # Copyright (c) 2014, Matt Layman
 
 import tempfile
-from unittest.runner import TextTestResult
+try:
+    from unittest.runner import TextTestResult
+except ImportError:
+    # Support Python 2.6.
+    from unittest import _TextTestResult as TextTestResult
 
 from tap.directive import Directive
 from tap.line import Result
@@ -15,5 +19,5 @@ class Factory(object):
             True, 1, 'This is a description.', Directive(directive_text))
 
     def make_test_result(self):
-        stream = tempfile.TemporaryFile()
+        stream = tempfile.TemporaryFile(mode='w')
         return TextTestResult(stream, None, 1)
