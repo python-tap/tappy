ef276f15b3cb823108bed12be59e271f66d300e9
Matt Layman <matthewlayman@gmail.com>
2015-08-13 22:01:06 -0400

    Collect pytest report results in the tracker.

--- tap/plugins/pytest.py
+++ tap/plugins/pytest.py
@@ -15,7 +15,7 @@ def pytest_addoption(parser):
         'An optional output directory to write TAP files to. '
         'If the directory does not exist, it will be created.'))
     # TODO: Add remaining options.
-    # TODO: Figure out how to suppress pytext terminal output.
+    # TODO: Figure out how to suppress pytest terminal output.
 
 
 def pytest_configure(config):
@@ -24,11 +24,21 @@ def pytest_configure(config):
     # TODO: Set options on tracker.
 
 
-def pytest_itemcollected(item):
-    """Track the test result."""
-    # TODO: Put the result into the tracker with proper description.
+def pytest_runtest_logreport(report):
+    """Add a test result to the tracker."""
+    if report.when != 'call':
+        return
+    description = report.location[2]
+    testcase = description.split('.', 1)[0]
+    if report.outcome == 'passed':
+        tracker.add_ok(testcase, description)
+    elif report.outcome == 'failed':
+        tracker.add_not_ok(testcase, description)
+    elif report.outcome == 'skipped':
+        reason = report.longrepr[2].split(':', 1)[1].strip()
+        tracker.add_skip(testcase, description, reason)
 
 
-def pytest_collectreport(report):
+def pytest_unconfigure(config):
     """Dump the results."""
-    # TODO: Dump out the results to files.
+    tracker.generate_tap_reports()
--- tap/tests/test_pytest_plugin.py
+++ tap/tests/test_pytest_plugin.py
@@ -16,7 +16,12 @@ class TestPytestPlugin(TestCase):
     def setUp(self):
         """The pytest plugin uses module scope so a fresh tracker
         must be installed each time."""
+        # When running this suite with pytest, save and restore the tracker.
+        self._tracker = pytest.tracker
         pytest.tracker = Tracker()
+
+    def tearDown(self):
+        pytest.tracker = self._tracker
 
     def test_includes_options(self):
         group = mock.Mock()
@@ -31,3 +36,42 @@ class TestPytestPlugin(TestCase):
         config.option.tap_outdir = outdir
         pytest.pytest_configure(config)
         self.assertEqual(pytest.tracker.outdir, outdir)
+
+    def test_track_when_call_report(self):
+        """Only the call reports are tracked."""
+        pytest.tracker = mock.Mock()
+        report = mock.Mock(when='setup', outcome='passed')
+        pytest.pytest_runtest_logreport(report)
+        self.assertFalse(pytest.tracker.add_ok.called)
+
+    def test_tracks_ok(self):
+        pytest.tracker = mock.Mock()
+        location = ('test_file.py', 1, 'TestFake.test_me')
+        report = mock.Mock(when='call', outcome='passed', location=location)
+        pytest.pytest_runtest_logreport(report)
+        pytest.tracker.add_ok.assert_called_once_with(
+            'TestFake', 'TestFake.test_me')
+
+    def test_tracks_not_ok(self):
+        pytest.tracker = mock.Mock()
+        location = ('test_file.py', 1, 'TestFake.test_me')
+        report = mock.Mock(when='call', outcome='failed', location=location)
+        pytest.pytest_runtest_logreport(report)
+        pytest.tracker.add_not_ok.assert_called_once_with(
+            'TestFake', 'TestFake.test_me')
+
+    def test_tracks_skip(self):
+        pytest.tracker = mock.Mock()
+        location = ('test_file.py', 1, 'TestFake.test_me')
+        longrepr = ('', '', 'Skipped: a reason')
+        report = mock.Mock(
+            when='call', outcome='skipped', location=location,
+            longrepr=longrepr)
+        pytest.pytest_runtest_logreport(report)
+        pytest.tracker.add_skip.assert_called_once_with(
+            'TestFake', 'TestFake.test_me', 'a reason')
+
+    def test_generates_reports(self):
+        pytest.tracker = mock.Mock()
+        pytest.pytest_unconfigure(None)
+        pytest.tracker.generate_tap_reports.assert_called_once_with()
