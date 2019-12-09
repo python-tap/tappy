90efbcb3263e86a17f2ae1cc9b58669185149b7d
Erik Cederstrand <erik@cederstrand.dk>
2019-12-09 16:35:54 +0100

    Ignore coverage on method that is only supportd on Python 3

--- tap/runner.py
+++ tap/runner.py
@@ -17,7 +17,7 @@ class TAPTestResult(TextTestResult):
     def __init__(self, stream, descriptions, verbosity):
         super(TAPTestResult, self).__init__(stream, descriptions, verbosity)
 
-    def addSubTest(self, test, subtest, err):
+    def addSubTest(self, test, subtest, err):  # pragma: no cover
         super(TAPTestResult, self).addSubTest(test, subtest, err)
         if err is not None:
             diagnostics = formatter.format_exception(err)
