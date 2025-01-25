842a6ff6e40131280ec1303e5224a1bf3f906087
Matt Layman <matthewlayman@gmail.com>
2025-01-25 13:00:28 -0500

    Fix the module test.

---
+++ src/tap/tests/test_example.py
@@ -0,0 +1,7 @@
+import unittest
+
+
+class TestCase(unittest.TestCase):
+    def test_it(self):
+        """This example exists for the test suite to have an in-package test."""
+        self.assertTrue(True)
